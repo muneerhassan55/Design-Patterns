b499dafd70d78b012ee116e842ffd4e77045a0f2
Muneer Hasan <muneerhassan5544@gmail.com>
2023-11-21 10:49:09 +0500

    file added
    file added

---
+++ facade_pattern/src/facade_pattern/Facade_pattern.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package facade_pattern;
+
+/**
+ *
+ * @author Muneer Hasan
+ */
+public class Facade_pattern {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
